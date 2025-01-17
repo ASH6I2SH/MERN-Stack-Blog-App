b2708cbc9285aa8cf96ab3b13cc43e0ed7ad39b2
Ashish Pandey <ash6i2sh@gmail.com>
2025-01-17 14:14:32 +0530

    front end completed

--- src/index.js
+++ src/index.js
@@ -11,14 +11,16 @@ import Home from './pages/Home'
 import PostDetail from './pages/PostDetail'
 import Register from './pages/Register'
 import Login from './pages/Login'
-import UserProfile from './pages/UserProfiile'
+import UserProfile from './pages/UserProfile'
 import Authors from './pages/Authors'
 import CreatePost from './pages/CreatePost'
 import EditPost from './pages/EditPost'
+import DeletePost from './pages/DeletePost'
 import CategoryPosts from './pages/CategoryPosts'
 import AuthorPosts from './pages/AuthorPosts'
 import Dashboard from './pages/Dashboard'
 import Logout from './pages/Logout'
+import { ImPriceTag } from 'react-icons/im';
 
 const router = createBrowserRouter([
   {
@@ -33,10 +35,12 @@ const router = createBrowserRouter([
       {path: "profile/:id", element: <UserProfile/>},
       {path: "authors", element: <Authors/>},
       {path: "create", element: <CreatePost/>},
+      
       {path: "posts/categories/:category", element: <CategoryPosts/>},
       {path: "posts/users/:id", element: <AuthorPosts/>},
       {path: "myposts/:id", element: <Dashboard/>},
       {path: "posts/:id/edit", element: <EditPost/>},
+      {path: "posts/:id/delete", element: <DeletePost/>},
       {path: "logout", element: <Logout/>}
     ]
   }
@@ -44,9 +48,11 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
-   <RouterProvider router = {router}/>
+    <RouterProvider router = {router}/>
   </React.StrictMode>
+  
 ); 
 
 
--- src/pages/UserProfiile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserProfiile = () => {
-  return (
-    <div>UserProfiile</div>
-  )
-}
-
-export default UserProfiile
